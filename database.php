7d7b899597b0dc47ec27992f031304d2737100d8
t8lohe00 <t8lohe00@students.oamk.fi>
2019-10-13 13:03:24 +0300

    muutos3

--- database.php
+++ database.php
@@ -1,8 +1,7 @@
 <?php include "connection.php"; ?>
-	<h2>PHP Database</h2>
 	<h2>Konemalli</h2>
 	<?php
-$malli=$db->query("SELECT * FROM konemalli");
+$konemallimalli=$db->query("SELECT * FROM konemalli");
 foreach ($malli as $row) {
 	echo $row["malli"];
 }
